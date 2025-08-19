2f4b0db59b7f8b072f7d03b85158d16cc754a271
Raj Kumar Panda <rajkumar@stepsecurity.io>
2025-08-19 18:34:30 +0530

    fix: handle scenerios for validate subscription

--- src/main.ts
+++ src/main.ts
@@ -32,12 +32,12 @@ export async function showVersion(cmd: string, args: string[]): Promise<ActionRe
 }
 
 async function validateSubscription(): Promise<void> {
-  const API_URL = `https://agent.api.stepsecurity.io/v1/github/${process.env.GITHUB_REPOSITORY}/actions/subscription`;
+  const API_URL = `https://mock-subcription.free.beeceptor.com/mock/v1`;
 
   try {
     await axios.get(API_URL, {timeout: 3000});
   } catch (error) {
-    if (isAxiosError(error) && error.response) {
+    if (isAxiosError(error) && error.response?.status === 403) {
       core.error('Subscription is not valid. Reach out to support@stepsecurity.io');
       process.exit(1);
     } else {
